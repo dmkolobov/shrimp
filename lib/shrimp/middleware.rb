ad46692045708bb170ef28afc8468143f1226016
Dmitry Kolobov <dmkolobov@gmail.com>
2013-06-06 19:30:19 -0600

    commented out

--- lib/shrimp/middleware.rb
+++ lib/shrimp/middleware.rb
@@ -11,119 +11,25 @@ module Shrimp
     end
 
     def call(env)
-      @request = Rack::Request.new(env)
-      status, headers, response = @app.call(env)
-
-      if render_as_pdf? #&& headers['Content-Type'] =~ /text\/html|application\/xhtml\+xml/
-        body = Phantom.new(@request.url.sub(%r{\.pdf$}, ''), @options, @request.cookies).to_pdf
-        response = [body]
-
-        # Do not cache PDFs
-        headers.delete('ETag')
-        headers.delete('Cache-Control')
-
-        headers["Content-Length"]         = (body.respond_to?(:bytesize) ? body.bytesize : body.size).to_s
-        headers["Content-Type"]           = "application/pdf"
-      end
+    #  @request = Rack::Request.new(env)
+    #  status, headers, response = @app.call(env)
+    #
+    #  if render_as_pdf? #&& headers['Content-Type'] =~ /text\/html|application\/xhtml\+xml/
+    #    body = Phantom.new(@request.url.sub(%r{\.pdf$}, ''), @options, @request.cookies).to_pdf
+    #    response = [body]
+    #
+    #    # Do not cache PDFs
+    #    headers.delete('ETag')
+    #    headers.delete('Cache-Control')
+    #
+    #    headers["Content-Length"]         = (body.respond_to?(:bytesize) ? body.bytesize : body.size).to_s
+    #    headers["Content-Type"]           = "application/pdf"
+    #  end
     end
 
     private
 
-    # Private: start phantom rendering in a separate process
-    def fire_phantom
-      Process::detach fork { Phantom.new(@request.url.sub(%r{\.pdf$}, ''), @options, @request.cookies).to_pdf(render_to) }
-    end
-
-    def render_to
-      file_name = Digest::MD5.hexdigest(@request.path) + ".pdf"
-      file_path = @options[:out_path]
-      "#{file_path}/#{file_name}"
-    end
-
-    def already_rendered?
-      File.exists?(render_to)
-    end
-
-    def up_to_date?(ttl = 30)
-      (Time.now - File.new(render_to).mtime) <= ttl
-    end
-
-
-    def remove_rendering_flag
-      @request.session["phantom-rendering"] ||={ }
-      @request.session["phantom-rendering"].delete(render_to)
-    end
-
-    def set_rendering_flag
-      @request.session["phantom-rendering"]            ||={ }
-      @request.session["phantom-rendering"][render_to] = Time.now
-    end
-
-    def rendering_timed_out?
-      Time.now - @request.session["phantom-rendering"][render_to] > @options[:request_timeout]
-    end
-
-    def rendering_in_progress?
-      @request.session["phantom-rendering"]||={ }
-      @request.session["phantom-rendering"][render_to]
-    end
-
     def render_as_pdf?
       !!@request.path.match(%r{\.pdf$})
     end
-
-    def concat(accepts, type)
-      (accepts || '').split(',').unshift(type).compact.join(',')
-    end
-
-    def reload_response(interval=1)
-      body = <<-HTML.gsub(/[ \n]+/, ' ').strip
-          <html>
-          <head>
-        </head>
-          <body onLoad="setTimeout(function(){ window.location.reload()}, #{interval * 1000});">
-          <h2>Prepareing pdf... </h2>
-          </body>
-        </ html>
-      HTML
-      headers                   = { }
-      headers["Content-Length"] = body.size.to_s
-      headers["Content-Type"]   = "text/html"
-      headers["Retry-After"]    = interval.to_s
-
-      [503, headers, [body]]
-    end
-
-    def ready_response
-      body = <<-HTML.gsub(/[ \n]+/, ' ').strip
-        <html>
-        <head>
-        </head>
-        <body>
-        <a href="#{@request.path}">PDF ready here</a>
-        </body>
-      </ html>
-      HTML
-      headers                   = { }
-      headers["Content-Length"] = body.size.to_s
-      headers["Content-Type"]   = "text/html"
-      [200, headers, [body]]
-    end
-
-    def error_response
-      body = <<-HTML.gsub(/[ \n]+/, ' ').strip
-        <html>
-        <head>
-        </head>
-        <body>
-        <h2>Sorry request timed out... </h2>
-        </body>
-      </ html>
-      HTML
-      headers                   = { }
-      headers["Content-Length"] = body.size.to_s
-      headers["Content-Type"]   = "text/html"
-      [504, headers, [body]]
-    end
-  end
 end
