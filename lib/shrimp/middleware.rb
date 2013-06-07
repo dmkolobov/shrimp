e3309eb21626d55c2b6f81057f7e10dcd54c12c4
Dmitry Kolobov <dmkolobov@gmail.com>
2013-06-06 19:53:36 -0600

    set request to render pdf

--- lib/shrimp/middleware.rb
+++ lib/shrimp/middleware.rb
@@ -6,9 +6,12 @@ module Shrimp
 
     def call(env)
       @request = Rack::Request.new(env)
+
+      set_request_to_render_as_pdf(env) if render_as_pdf?
+
       status, headers, response = @app.call(env)
 
-      if render_as_pdf? #&& headers['Content-Type'] =~ /text\/html|application\/xhtml\+xml/
+      if render_as_pdf? && headers['Content-Type'] =~ /text\/html|application\/xhtml\+xml/
         body = Phantom.new(@request.url.sub(%r{\.pdf$}, ''), {}, @request.cookies).to_pdf
         response = [body]
 
@@ -26,6 +29,13 @@ module Shrimp
 
     def render_as_pdf?
       !!@request.path.match(%r{\.pdf$})
+    end
+
+    def set_request_to_render_as_pdf(env)
+      path = @request.path.sub(%r{\.pdf$}, '')
+      %w[PATH_INFO REQUEST_URI].each { |e| env[e] = path }
+      env['HTTP_ACCEPT'] = concat(env['HTTP_ACCEPT'], Rack::Mime.mime_type('.html'))
+      env["Rack-Middleware-PDFKit"] = "true"
     end
   end
 end
