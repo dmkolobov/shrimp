a6ce81d676f6688051bb67b35f6f4105f831ee69
Dmitry Kolobov <dmkolobov@gmail.com>
2013-06-06 19:48:06 -0600

    Shrimp.

--- lib/shrimp/middleware.rb
+++ lib/shrimp/middleware.rb
@@ -5,22 +5,21 @@ module Shrimp
     end
 
     def call(env)
+      @request = Rack::Request.new(env)
       status, headers, response = @app.call(env)
-    #  @request = Rack::Request.new(env)
-    #  status, headers, response = @app.call(env)
-    #
-    #  if render_as_pdf? #&& headers['Content-Type'] =~ /text\/html|application\/xhtml\+xml/
-    #    body = Phantom.new(@request.url.sub(%r{\.pdf$}, ''), @options, @request.cookies).to_pdf
-    #    response = [body]
-    #
-    #    # Do not cache PDFs
-    #    headers.delete('ETag')
-    #    headers.delete('Cache-Control')
-    #
-    #    headers["Content-Length"]         = (body.respond_to?(:bytesize) ? body.bytesize : body.size).to_s
-    #    headers["Content-Type"]           = "application/pdf"
-    #  end
-      [status, headers, response.body]
+
+      if render_as_pdf? #&& headers['Content-Type'] =~ /text\/html|application\/xhtml\+xml/
+        body = Phantom.new(@request.url.sub(%r{\.pdf$}, ''), {}, @request.cookies).to_pdf
+        response = [body]
+
+        # Do not cache PDFs
+        headers.delete('ETag')
+        headers.delete('Cache-Control')
+
+        headers["Content-Length"]         = (body.respond_to?(:bytesize) ? body.bytesize : body.size).to_s
+        headers["Content-Type"]           = "application/pdf"
+      end
+      [status, headers, response]
     end
 
     private
