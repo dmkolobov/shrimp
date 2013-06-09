faf1aba3935e597744097d676e4cf05039cb8072
Dmitry Kolobov <dmkolobov@gmail.com>
2013-06-08 22:47:08 -0600

    Remove useless logger thing.

--- lib/shrimp/phantom.rb
+++ lib/shrimp/phantom.rb
@@ -31,7 +31,6 @@ module Shrimp
     # Returns the stdout output of phantomjs
     def run
       @error  = nil
-      Rails.logger.debug cmd
       @result = `#{cmd}`
       unless $?.exitstatus == 0
         @error  = @result
